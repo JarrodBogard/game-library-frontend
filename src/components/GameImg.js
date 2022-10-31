751223a5acce25d4d186d3524c551cc5ffdd286a
Jarrod Bogard <jarrodbogard@gmail.com>
2022-10-31 11:22:33 -0500

    updated function to handle buffer data for image

--- src/components/GameImg.js
+++ src/components/GameImg.js
@@ -1,17 +1,24 @@
+import { useEffect, useState } from "react";
 const GameImg = ({ game }) => {
-  const base64String = btoa(
-    String.fromCharCode(...new Uint8Array(game.img.data.data))
-  );
-  console.log(game);
+  const [state, setState] = useState("");
+
+  useEffect(() => {
+    const base64String = btoa(
+      new Uint8Array(game.img.data.data).reduce(function (data, byte) {
+        return data + String.fromCharCode(byte);
+      }, "")
+    );
+    // const base64String = btoa(
+    //   String.fromCharCode(...new Uint8Array(game.img.data.data))
+    // );
+
+    setState(base64String);
+  }, [game]);
 
   return (
     <img
       style={{ width: "200px", height: "200px", alignSelf: "center" }}
-      src={
-        game &&
-        game.img &&
-        `data:${game.img.contentType};base64,${base64String}`
-      }
+      src={game && game.img && `data:${game.img.contentType};base64,${state}`}
       alt={`Cover: ${game.title}`}
     />
   );
