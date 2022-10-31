6040e3fc0ba847b941edc874f6e32426839f3df0
Jarrod Bogard <jarrodbogard@gmail.com>
2022-10-31 18:06:13 -0500

    added games dispatch to logout to clear games when user logs out

--- src/contexts/GameContext.js
+++ src/contexts/GameContext.js
@@ -1,5 +1,4 @@
 import { createContext, useReducer } from "react";
-// import { useAuthContext } from "../hooks/useAuthContext";
 
 export const GameContext = createContext();
 
@@ -21,18 +20,9 @@ export const gameReducer = (state, action) => {
 };
 
 export const GameContextProvider = ({ children }) => {
-  //   const { user } = useAuthContext();
   const [state, dispatch] = useReducer(gameReducer, {
     games: null,
   });
-
-  //   useEffect(() => {
-  //     const games = JSON.parse(localStorage.getItem("games"));
-
-  //     if (user) {
-  //       dispatch({ type: "SET_GAMES", payload: games });
-  //     }
-  //   }, [user]);
 
   // console.log("GameContext state: ", state);
 
--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,12 +1,16 @@
 import { useAuthContext } from "./useAuthContext";
+import { useGameContext } from "./useGameContext";
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: gamesDispatch } = useGameContext();
 
   const logout = () => {
     localStorage.removeItem("user");
 
     dispatch({ type: "LOGOUT" });
+    gamesDispatch({ type: "SET_GAMES", payload: null });
   };
+
   return { logout };
 };
